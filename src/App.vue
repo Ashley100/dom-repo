bf52a5ef831c5acf896e354c6a7e6ef587a243d3
Piter <to20099@gmail.com>
2020-03-01 22:36:57 +0200

    commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>asdasd</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, rerum. Temporibus, neque iste dignissimos vitae ex deserunt at exercitationem possimus aliquid atque harum quos, autem perferendis cupiditate asperiores eveniet provident!</p>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
